d8f28158c7c4473ed619217a3940fe2401001fde
Renardo Alves <61791273+RenardoAlves@users.noreply.github.com>
2026-04-30 22:16:02 -0300

    Implementada lógica para definir nome do arquivo

--- src/downloader.c
+++ src/downloader.c
@@ -2,7 +2,7 @@
 
 #include "downloader.h"
 
-int download_file (const char *url) {
-    printf("Downloading: %s\n", url);
+int download_file(const char *url, const char *filename) {
+    printf("Downloading: %s to %s\n", url, filename);
     return 0;
 }
--- src/main.c
+++ src/main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "downloader.h"
 
-int main (int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("Usage: %s <URL>\n", argv[0]);
+int main(int argc, char *argv[]) {
+    if (argc < 2 || argc > 3) {
+        printf("Usage: %s <URL> [filename]\n", argv[0]);
         return 1;
     }
 
-    return download_file(argv[1]);
+    const char *url = argv[1];
+    const char *filename = NULL;
+
+    if (argc == 3) {
+        filename = argv[2];
+    } else {
+        filename = strrchr(url, '/');
+        if (filename && *(filename + 1) != '\0') {
+            filename++; // Skip the slash
+        } else {
+            filename = "downloaded_file"; // Fallback if no slash or slash is at the end
+        }
+    }
+
+    return download_file(url, filename);
 }
