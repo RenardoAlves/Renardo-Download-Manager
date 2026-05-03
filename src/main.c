a851b57b2ebde93743c2d449e3a083d2ce620914
Renardo Alves <61791273+RenardoAlves@users.noreply.github.com>
2026-05-03 02:43:21 -0300

    Desabilitado QuickEdit enquanto o programa roda
    Para evitar que o download seja pausado quando o usuário clicar no terminal (Windows)

--- src/main.c
+++ src/main.c
@@ -4,7 +4,20 @@
 
 #include "downloader.h"
 
+#ifdef _WIN32
+#include <windows.h>
+void disable_quick_edit() {
+    HANDLE hInput = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD prev_mode;
+    GetConsoleMode(hInput, &prev_mode);
+    SetConsoleMode(hInput, (prev_mode & ~ENABLE_QUICK_EDIT_MODE) | ENABLE_EXTENDED_FLAGS);
+}
+#endif
+
 int main(int argc, char *argv[]) {
+#ifdef _WIN32
+    disable_quick_edit();
+#endif
 
     curl_global_init(CURL_GLOBAL_DEFAULT);
 
